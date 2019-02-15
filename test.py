d5eb4d31bab0bf4aaf6450549147ebe6aa53bb5e
jwongUlti25 <jwong25@stuy.edu>
2019-02-15 13:35:50 -0500

    pushed testes

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+import numpy as np
+
+#import matplotlib.pyplot as plt
+
+#f = open('game.csv', 'r')
+#for i in f:
+#    print(i)
+
+s = pd.Series([1,3,5,np.nan,6,8])
+print(s)
