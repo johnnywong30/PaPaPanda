89cf982132fce49b21c6835afe25946d0a0fb3f5
jwongUlti25 <jwong25@stuy.edu>
2019-03-17 16:31:47 -0400

    start present

---
+++ present/present.py
@@ -0,0 +1,19 @@
+# PaPaPandas - Jack Lu & Johnny Wong
+# SoftDev2 pd8
+# 2019-03-18
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Data structures
+# series
+s = pd.Series(5, index=['a', 'b', 'c'])
+#print(s)
+
+# dataframe
+
+
+# pokedex presentation
+pokedex = pd.read_json('pokedex.json')
+print(pokedex)
