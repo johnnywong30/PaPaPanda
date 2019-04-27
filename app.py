f2288d7c534392e88a9d82586a97b90e64076eb6
jwongUlti25 <jwong25@stuy.edu>
2019-04-26 23:05:40 -0400

    run app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ P#03 -- You Are the Expert: TEDxSoftDev 2019
 
 import os
 
-from flask import Flask, render_template, redirect, url_for, session, request,
+from flask import Flask, render_template, redirect, url_for, session, request
 
 #==========instantiate Flask object=====================
 app = Flask(__name__)
@@ -16,3 +16,7 @@ app.secret_key = os.urandom(32)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
