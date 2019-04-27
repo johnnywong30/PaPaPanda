6eefd600264d01e9589523744398154cc5370c96
jwongUlti25 <jwong25@stuy.edu>
2019-04-26 22:47:07 -0400

    started website

---
+++ app.py
@@ -0,0 +1,18 @@
+'''
+Team PaPaPandas - Johnny Wong and Jack Lu
+SoftDev2 pd8
+P#03 -- You Are the Expert: TEDxSoftDev 2019
+2019-04-30
+'''
+
+import os
+
+from flask import Flask, render_template, redirect, url_for, session, request,
+
+#==========instantiate Flask object=====================
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
---
+++ present/panda.py
@@ -0,0 +1,3 @@
+# PaPaPanda - Jack Lu & Johnny Wong
+# SoftDev2 pd8
+# 2019-03-18
